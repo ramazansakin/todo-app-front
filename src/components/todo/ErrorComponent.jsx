00ad752fb1f1ab7513408cb039f95216bcf23749
r84160880 <ramazan.sakin@huawei.com>
2020-06-29 14:14:24 +0300

    Authentication&Authorization components added

---
+++ src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ErrorComponent() {
+    return <div>An Error Occurred. I don't know what to do! Contact support at Bla-Bla...</div>
+}
+
+export default ErrorComponent
